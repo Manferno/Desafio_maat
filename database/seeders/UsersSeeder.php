8f356b2707413b8215f48b2e204268e7b27fdc07
Manferno <manuel.rcb.mc@gmail.com>
2023-06-19 14:51:51 -0400

    Lo que quedo

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+   
+    
+    public function run(): void
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'password' => bcrypt('password123'),
+        ]);     
+    }
+}
+
